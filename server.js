d06c79469043b297e26aa5be54addedf713cc27a
Fernando Santana <fernandosantanajr@gmail.com>
2021-11-30 00:15:04 -0300

    projeto das aulas

---
+++ server.js
@@ -0,0 +1,15 @@
+var fs = require('fs'),
+    http = require('http');
+ 
+http.createServer(function (req, res) {
+  console.log(req.url)
+  fs.readFile(__dirname+'/public' + req.url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(5000);
